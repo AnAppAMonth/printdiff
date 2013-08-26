b00b1413b13d594884d4a682496895ae6991543b
Feng Qiu <feng@ban90.com>
2013-08-26 09:00:55 +0800

    Before extracting context lines code to another function
    Pre-context lines tests currently pass.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 var util = require('util');
 var objectDiff = require('objectdiff');
+var charDiff = require('chardiff');
+
+var consoleColumns = process.stdout.columns;
+
+var red = '\x1B[31m',
+    green = '\x1B[32m',
+    cyan = '\x1B[36m',
+    clear = '\x1B[0m';
 
 /**
  * This function takes a string to be printed to the console, and formats
@@ -11,17 +19,16 @@ var objectDiff = require('objectdiff');
  * string, but not in the printed output. So this function must be able
  * to deal with them.
  *
- * @param str {string} the input string.
+ * @param {string} str - the input string.
  * @returns {string} the output string.
  * @private
  */
-function _lineBreak(str) {
+function _lineBreak(str, columns) {
     var result = '';
-    var consoleColumns = process.stdout.columns;
 
     // As an optimization, if the length of str (including color sequences)
     // doesn't exceed the console's width, this is definitely an one-liner.
-    if (str.length <= consoleColumns) {
+    if (str.length <= columns) {
         return str;
     }
 
@@ -41,7 +48,7 @@ function _lineBreak(str) {
             }
         } else {
             ct++;
-            if (str[i] === '\n' || ct === consoleColumns) {
+            if (str[i] === '\n' || ct === columns) {
                 // We have finished counting a line.
                 if (start) {
                     // This isn't the first line, prepend '\n' and 4 spaces.
@@ -49,7 +56,7 @@ function _lineBreak(str) {
                 } else {
                     // This is the first line, all subsequent lines must leave
                     // space for the 4 spaces prepended.
-                    consoleColumns -= 4;
+                    columns -= 4;
                 }
                 // Add the line to the result.
                 if (str[i] === '\n') {
@@ -75,14 +82,221 @@ function _lineBreak(str) {
     return result;
 }
 
+function _printLine(line, prefix, columns, style) {
+    if (prefix.length >= 3) {
+        prefix += ' ';
+    } else {
+        prefix += new Array(5 - prefix.length).join(' ');
+    }
+
+    columns -= prefix.length;
+    if (line.length > columns) {
+        line = line.substring(0, columns - 3) + '...';
+    }
+
+    line = prefix + line;
+
+    if (style) {
+        line = style + line + clear;
+    }
+
+    return line;
+}
+
+function _printColumns() {
+
+}
+
+// Returns the new `lastPrintedLine`.
+function _printContextLines(result, lines, curLine, lastPrintedLine, postContextLine, contextLines) {
+    var startLine, endLine,
+        i;
+
+    // Print post-context lines for the previous change if existed.
+    if (postContextLine >= 0) {
+        // Print context lines `postContextLine` ~ `postContextLine+contextLines-1`
+        // in `a` if necessary.
+        startLine = Math.max(postContextLine, lastPrintedLine + 1);
+        endLine = Math.min(postContextLine + contextLines, curLine);
+        for (i = startLine; i < endLine; i++) {
+            result.push(_printLine(lines[i], i + 1 + '', columns, null));
+        }
+    }
+
+    // Print context lines `curLine-contextLines` ~ `curLine-1` in `a`
+    // if necessary.
+    startLine = Math.max(curLine - contextLines, lastPrintedLine + 1, 0);
+    for (i = startLine; i < curLine; i++) {
+        result.push(_printLine(lines[i], i + 1 + '', columns, null));
+    }
+
+    return lastPrintedLine;
+}
+
+/**
+ * This function uses the `diff` library to diff two strings and generates a
+ * nice-looking diff (inspired by file diffs) from the result.
+ *
+ * The difference between this function and the file diff algorithm is that
+ * this function takes the first operand (`a`) as the base and outputs what
+ * changes are to be made to convert `a` to `b`. As a result, we don't care
+ * about line and column numbers in `b`: all line and column numbers in the
+ * output are those in `a`.
+ *
+ * @param {string} a - diff operand 1.
+ * @param {string} b - diff operand 2.
+ * @returns {Array} empty if equal, otherwise a list of changes.
+ * @private
+ */
+function _generateStringDiff(a, b, columns) {
+    // A "line" is a line in one of the operands.
+    // A "chunk" is a displayed line on the screen, limited by tty's width.
+    var maxChunks = 200,
+        maxChunksPerLine = 20,
+        contextLines = 3,
+        contextColumns = 25,
+        maxColumns = 50;
+
+    // Context lines are fetched from `lines`.
+    var lines = a.split('\n'),
+        i, j, k, ln;
+
+    var result = [],
+        // The current (upcoming) line number in `a`.
+        curLine = 0,
+        // Line number of the last printed line in `a`.
+        lastPrintedLine = -1,
+        // The first post-context line from the last change, we need to record
+        // this because we can only print it when we are at the next change.
+        postContextLine = -1,
+        // The total number of chunks printed so far, used to enforce the
+        // `maxChunks` setting.
+        totalChunks = 0,
+        startLine;
+
+    // Holds char-level result before merging into `result`.
+    var colRes,
+        // The current (upcoming) column number in the current line in 'a'.
+        curColumn,
+        // Column number of the last printed column in the current line in `a`.
+        lastPrintedColumn,
+        // The first post-context column from the last change, we need to record
+        // this because we can only print it when we are at the next change.
+        postContextColumn,
+        // The number of chunks printed so far for the current line, used to
+        // enforce the `maxChunksPerLine` setting.
+        lineChunks = 0,
+        startColumn;
+
+    // Format strings
+    var changeStr = [];
+    var removeStr = ['-   %s',
+                     '\x1B[31m-   %s\x1B[0m'];
+    var addStr = ['+   %s',
+                  '\x1B[32m+   %s\x1B[0m'];
+//    var idx = colors ? 1 : 0;
+
+
+    var changeset = charDiff(a, b);
+
+    for (i = 0; i < changeset.length; i++) {
+        var change = changeset[i];
+
+        if (change.type === '=') {      // Unchanged
+            curLine++;
+        } else if (change.type === '-') {   // Removed
+            // Print context lines `curLine-contextLines` ~ `curLine-1` in `a`
+            // if necessary.
+            startLine = Math.max(curLine - contextLines, lastPrintedLine + 1, 0);
+            for (j = startLine; j < curLine; j++) {
+                result.push(_printLine(lines[j], j + 1 + '', columns, null));
+            }
+
+            // Print the removed line.
+            result.push(_printLine(lines[curLine], curLine + 1 + '', columns, red));
+
+            // Update `lastPrintedLine`.
+            lastPrintedLine = curLine;
+
+            curLine++;
+
+        } else if (change.type === '+') {   // Added
+            // Print context lines `curLine-contextLines` ~ `curLine-1` in `a`
+            // if necessary.
+            startLine = Math.max(curLine - contextLines, lastPrintedLine + 1, 0);
+            for (j = startLine; j < curLine; j++) {
+                result.push(_printLine(lines[j], j + 1 + '', columns, null));
+            }
+
+            // Print the added line, strip the trailing line break if existed.
+            ln = change.right;
+            if (ln[ln.length - 1] === '\n') {
+                ln = ln.substring(0, ln.length - 1);
+            }
+            result.push(_printLine(ln, '', columns, green));
+
+            // Update `lastPrintedLine`.
+            lastPrintedLine = curLine - 1;
+
+        } else {    // Changed
+            // Print context lines `curLine-contextLines` ~ `curLine-1` in `a`
+            // if necessary.
+            startLine = Math.max(curLine - contextLines, lastPrintedLine + 1, 0);
+            for (j = startLine; j < curLine; j++) {
+                result.push(_printLine(lines[j], j + 1 + '', columns, null));
+            }
+
+            // Process char-level diffs
+            colRes = [];
+            curColumn = 0;
+            lastPrintedColumn = -1;
+            postContextColumn = -1;
+            for (j = 0; j < change.diff.length; j++) {
+                var chg = change.diff[j];
+
+                if (chg.type === '=') {
+                    curColumn++;
+                } else if (chg.type === '-') {
+                    curColumn++;
+
+                } else if (chg.type === '+') {
+                    curColumn++;
+
+                } else {
+                    curColumn++;
+
+                }
+            }
+//            result.push(colRes.join(''));
+
+            // Print the removed line.
+            result.push(_printLine(lines[curLine], curLine + 1 + '', columns, red));
+
+            // Print the added line, strip the trailing line break if existed.
+            ln = change.right;
+            if (ln[ln.length - 1] === '\n') {
+                ln = ln.substring(0, ln.length - 1);
+            }
+            result.push(_printLine(ln, '', columns, green));
+
+            // Update `lastPrintedLine`.
+            lastPrintedLine = curLine;
+
+            curLine++;
+        }
+    }
+
+    return result;
+}
+
 /**
  * This function uses the objectdiff library to diff two objects or
  * literals and generates a nice-looking diff (inspired by file diffs)
  * from the result.
  *
- * @param a {*} diff operand 1, can be object or literal.
- * @param b {*} diff operand 2, can be object or literal.
- * @param colors {boolean} whether to colorize the output.
+ * @param {*} a - diff operand 1, can be object or literal.
+ * @param {*} b - diff operand 2, can be object or literal.
+ * @param {boolean} colors - whether to colorize the output.
  * @returns {Array} empty if equal, otherwise a list of changes.
  * @private
  */
@@ -124,17 +338,17 @@ function _generateObjectDiff(a, b, colors) {
             result.push(_lineBreak(util.format(changeStr[idx]
                                              , path
                                              , util.inspect(node.removed)
-                                             , util.inspect(node.added))));
+                                             , util.inspect(node.added)), consoleColumns));
 
         } else if (node.changed === 'removed') {
             result.push(_lineBreak(util.format(removeStr[idx]
                                              , path
-                                             , util.inspect(node.value))));
+                                             , util.inspect(node.value)), consoleColumns));
 
         } else if (node.changed === 'added') {
             result.push(_lineBreak(util.format(addStr[idx]
                                              , path
-                                             , util.inspect(node.value))));
+                                             , util.inspect(node.value)), consoleColumns));
         }
     }
 
@@ -144,11 +358,14 @@ function _generateObjectDiff(a, b, colors) {
 
         traverse(diff, '');
 
+    } else if (typeof a === 'string' && typeof b === 'string') {
+        result = result.concat(_generateStringDiff(a, b, consoleColumns));
+
     } else {
         if (a !== b) {
             result.push(_lineBreak(util.format(changeStr2[idx]
                                              , util.inspect(a)
-                                             , util.inspect(b))));
+                                             , util.inspect(b)), consoleColumns));
         }
     }
 
---
+++ test/string.js
@@ -0,0 +1,55 @@
+
+var chai = require('chai'),
+    expect = chai.expect,
+    chaiCommon = require('chai-common'),
+    chaiHighlight = require('chai-highlight'),
+    printdiff = require('../');
+
+chai.use(chaiCommon);
+chai.use(chaiHighlight);
+chaiHighlight.setStyles('\x1B[36m', '\x1B[31m');
+
+var savedLog = console.log,
+    stdout;
+
+function patchedLog(str) {
+    stdout += str + '\n';
+}
+
+function patchedPrintDiff(a, b) {
+    stdout = '';
+    console.log = patchedLog;
+    printdiff(a, b);
+    console.log = savedLog;
+}
+
+// Because mocha also uses stdout, this guarantees our output begins at a fresh line.
+function LOG(str) {
+    console.log('\n' + str);
+}
+
+describe('When diffing strings,', function() {
+    describe('pre-context lines', function() {
+        it('should be fully displayed when in the middle of the text', function() {
+            var a = 'a\nb\nc\nd\ne\nf\ng\nh\n',
+                b = 'a\nb\nc\nd\nex\nf\ng\nh\n';
+
+            patchedPrintDiff(a, b);
+            expect(stdout).to.startWith('2   b\n3   c\n4   d\n\x1B[31m5   e\x1B[0m\n');
+        });
+        it('should partly be displayed when near text boundary', function() {
+            var a = 'a\nb\nc\nd\ne\nf\ng\nh\n',
+                b = 'a\nb\ncx\nd\ne\nf\ng\nh\n';
+
+            patchedPrintDiff(a, b);
+            expect(stdout).to.startWith('1   a\n2   b\n\x1B[31m3   c\x1B[0m\n');
+        });
+        it('should not be displayed at all when at text boundary', function() {
+            var a = 'a\nb\nc\nd\ne\nf\ng\nh\n',
+                b = 'ax\nb\nc\nd\ne\nf\ng\nh\n';
+
+            patchedPrintDiff(a, b);
+            expect(stdout).to.startWith('\x1B[31m1   a\x1B[0m\n');
+        });
+    });
+});
